0474da3decc3f8bbfb49fe805f034d2df9969618
Silvio Traversaro <silvio@traversaro.it>
2024-10-02 09:39:28 +0200

    Update build_py.sh

--- recipe/build_py.sh
+++ recipe/build_py.sh
@@ -6,6 +6,8 @@ rm -rf build
 mkdir build
 cd build
 
+# Workaround for SP_DIR value that somestimes as of October 2024 seems wrong
+Python_SP_DIR="$(python -c 'import sysconfig; print(sysconfig.get_path("purelib"))')"
 cmake ${CMAKE_ARGS} -GNinja .. \
     -DCMAKE_BUILD_TYPE=Release \
     -DBUILD_SHARED_LIBS:BOOL=ON \
@@ -14,7 +16,7 @@ cmake ${CMAKE_ARGS} -GNinja .. \
     -DCREATE_RUBY:BOOL=OFF \
     -DCREATE_JAVA:BOOL=OFF \
     -DCREATE_CSHARP:BOOL=OFF \
-    -DCMAKE_INSTALL_PYTHONDIR:PATH=${SP_DIR}
+    -DCMAKE_INSTALL_PYTHONDIR:PATH=${Python_SP_DIR}
 
 ninja -v
 cmake --build . --config Release --target install
